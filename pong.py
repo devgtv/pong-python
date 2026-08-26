480bf80935efebf754a00440251a0af4da91cac6
Gustavo Fialho <166181472+devgtv@users.noreply.github.com>
2026-08-26 10:00:56 -0300

    feat: add ball with movement and wall bouncing (#7)

--- pong.py
+++ pong.py
@@ -16,6 +16,11 @@ paddle_speed = 6
 player1 = pygame.Rect(50, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
 player2 = pygame.Rect(WIDTH - 50 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
 
+BALL_SIZE = 15
+ball = pygame.Rect(WIDTH // 2 - BALL_SIZE // 2, HEIGHT // 2 - BALL_SIZE // 2, BALL_SIZE, BALL_SIZE)
+ball_speed_x = 5
+ball_speed_y = 5
+
 clock = pygame.time.Clock()
 
 while True:
@@ -34,8 +39,15 @@ while True:
     if keys[pygame.K_DOWN] and player2.bottom < HEIGHT:
         player2.y += paddle_speed
 
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= HEIGHT:
+        ball_speed_y *= -1
+
     screen.fill(BLACK)
     pygame.draw.rect(screen, WHITE, player1)
     pygame.draw.rect(screen, WHITE, player2)
+    pygame.draw.ellipse(screen, WHITE, ball)
     pygame.display.flip()
     clock.tick(60)
