ac2401fa3d01ce98579f2135b2e103d19419389d
devgtv <gustavobarrosfialho1402gb@gmail.com>
2026-08-26 09:54:56 -0300

    feat: add paddles with player controls

--- pong.py
+++ pong.py
@@ -10,6 +10,12 @@ pygame.display.set_caption("Pong")
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+PADDLE_WIDTH, PADDLE_HEIGHT = 15, 100
+paddle_speed = 6
+
+player1 = pygame.Rect(50, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+player2 = pygame.Rect(WIDTH - 50 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
 clock = pygame.time.Clock()
 
 while True:
@@ -18,6 +24,18 @@ while True:
             pygame.quit()
             sys.exit()
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] and player1.top > 0:
+        player1.y -= paddle_speed
+    if keys[pygame.K_s] and player1.bottom < HEIGHT:
+        player1.y += paddle_speed
+    if keys[pygame.K_UP] and player2.top > 0:
+        player2.y -= paddle_speed
+    if keys[pygame.K_DOWN] and player2.bottom < HEIGHT:
+        player2.y += paddle_speed
+
     screen.fill(BLACK)
+    pygame.draw.rect(screen, WHITE, player1)
+    pygame.draw.rect(screen, WHITE, player2)
     pygame.display.flip()
     clock.tick(60)
